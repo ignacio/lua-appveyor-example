c5f19c05800a4e75a20f6631269e516863665470
Ignacio Burgueño <ignaciob@inconcertcc.com>
2015-03-22 20:56:20 -0300

    wrap

--- .appveyor/install.bat
+++ .appveyor/install.bat
@@ -20,9 +20,9 @@ appveyor DownloadFile %LUAROCKS_URL%/luarocks-%LUAROCKS_VER%-win32.zip
 7z x luarocks-%LUAROCKS_VER%-win32.zip
 cd luarocks-%LUAROCKS_VER%-win32
 call install.bat /LUA c:\lua%LUA_VER% /Q /LV %LUA_SHORTV%
-set PATH=%PATH%;%ProgramFiles(x86)%\LuaRocks\%LUAROCKS_SHORTV%\
+set PATH=%PATH%;%ProgramFiles(x86)%\LuaRocks\%LUAROCKS_SHORTV%\;%ProgramFiles(x86)%\LuaRocks\systree\bin
 set LUA_PATH=%ProgramFiles(x86)%\LuaRocks\%LUAROCKS_SHORTV%\lua\?.lua;%ProgramFiles(x86)%\LuaRocks\%LUAROCKS_SHORTV%\lua\?\init.lua;%ProgramFiles(x86)%\LuaRocks\systree\share\lua\%LUA_SHORTV%\?.lua;%ProgramFiles(x86)%\LuaRocks\systree\share\lua\%LUA_SHORTV%\?\init.lua
 set LUA_CPATH=%ProgramFiles(x86)%\LuaRocks\systree\lib\lua\%LUA_SHORTV%\?.dll
 call luarocks --version
-:: Install lunitx
-luarocks install lunitx
+:: Install busted, but you can pick any other test library
+call luarocks install busted
