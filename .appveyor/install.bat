30c1d05d90b48b3c2c6fa7c90a0d43ebc92d94d8
Ignacio Burgueño <ignaciob@inconcertcc.com>
2015-03-22 20:22:00 -0300

    work 6

--- .appveyor/install.bat
+++ .appveyor/install.bat
@@ -13,14 +13,16 @@ call etc\winmake.bat install c:\lua%LUA_VER%
 :: defines LUA_DIR so Cmake can find this Lua install
 set LUA_DIR=c:\lua%LUA_VER%
 set PATH=%PATH%;c:\lua%LUA_VER%\bin
+lua -v
 :: Downloads and installs LuaRocks
 cd %APPVEYOR_BUILD_FOLDER%
 appveyor DownloadFile %LUAROCKS_URL%/luarocks-%LUAROCKS_VER%-win32.zip
 7z x luarocks-%LUAROCKS_VER%-win32.zip
 cd luarocks-%LUAROCKS_VER%-win32
 call install.bat /LUA c:\lua%LUA_VER% /Q /LV %LUA_SHORTV%
-set PATH="%PATH%;%ProgramFiles(x86)%\LuaRocks\%LUAROCKS_SHORTV%"
+set PATH=%ProgramFiles(x86)%\LuaRocks\%LUAROCKS_SHORTV%\;%PATH%
 echo PATH: %PATH%
+luarocks
 set LUA_PATH="%ProgramFiles(x86)%\LuaRocks\%LUAROCKS_SHORTV%\lua\?.lua;%ProgramFiles(x86)%\LuaRocks\%LUAROCKS_SHORTV%\lua\?\init.lua;%ProgramFiles(x86)%\LuaRocks\systree\share\lua\%LUA_SHORTV%\?.lua;%ProgramFiles(x86)%\LuaRocks\systree\share\lua\%LUA_SHORTV%\?\init.lua"
 set LUA_CPATH="%ProgramFiles(x86)%\LuaRocks\systree\lib\lua\%LUA_SHORTV%\?.dll"
 luarocks --version
