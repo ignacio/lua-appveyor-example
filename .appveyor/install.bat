5ed697513d0b332aeb8fdab52681ab78248b3904
Ignacio Burgueño <ignaciob@inconcertcc.com>
2015-03-22 20:31:54 -0300

    work 6

--- .appveyor/install.bat
+++ .appveyor/install.bat
@@ -20,11 +20,9 @@ appveyor DownloadFile %LUAROCKS_URL%/luarocks-%LUAROCKS_VER%-win32.zip
 7z x luarocks-%LUAROCKS_VER%-win32.zip
 cd luarocks-%LUAROCKS_VER%-win32
 call install.bat /LUA c:\lua%LUA_VER% /Q /LV %LUA_SHORTV%
-set PATH=%ProgramFiles(x86)%\LuaRocks\%LUAROCKS_SHORTV%\;%PATH%
-echo PATH: %PATH%
-luarocks
-set LUA_PATH="%ProgramFiles(x86)%\LuaRocks\%LUAROCKS_SHORTV%\lua\?.lua;%ProgramFiles(x86)%\LuaRocks\%LUAROCKS_SHORTV%\lua\?\init.lua;%ProgramFiles(x86)%\LuaRocks\systree\share\lua\%LUA_SHORTV%\?.lua;%ProgramFiles(x86)%\LuaRocks\systree\share\lua\%LUA_SHORTV%\?\init.lua"
-set LUA_CPATH="%ProgramFiles(x86)%\LuaRocks\systree\lib\lua\%LUA_SHORTV%\?.dll"
+set PATH=%PATH%;%ProgramFiles(x86)%\LuaRocks\%LUAROCKS_SHORTV%\
+set LUA_PATH=%ProgramFiles(x86)%\LuaRocks\%LUAROCKS_SHORTV%\lua\?.lua;%ProgramFiles(x86)%\LuaRocks\%LUAROCKS_SHORTV%\lua\?\init.lua;%ProgramFiles(x86)%\LuaRocks\systree\share\lua\%LUA_SHORTV%\?.lua;%ProgramFiles(x86)%\LuaRocks\systree\share\lua\%LUA_SHORTV%\?\init.lua
+set LUA_CPATH=%ProgramFiles(x86)%\LuaRocks\systree\lib\lua\%LUA_SHORTV%\?.dll
 luarocks --version
 :: Install lunitx
 luarocks install lunitx
