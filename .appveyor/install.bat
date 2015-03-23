68d6fb90859e435e9775a97dcd8ba003865b4d6a
Ignacio Burgueño <ignaciob@inconcertcc.com>
2015-03-23 14:36:17 -0300

    Adds LuaJIT 2.0.3 and 2.1 to the build matrix.
    Now the matrix consists of:
    - Lua 5.1
    - Lua 5.2
    - Lua 5.3
    - LuaJIT 2.0.3
    - LuaJIT 2.1 (HEAD)

--- .appveyor/install.bat
+++ .appveyor/install.bat
@@ -1,34 +1,64 @@
+Setlocal EnableDelayedExpansion
+
 cd %APPVEYOR_BUILD_FOLDER%
 
-:: Make VS 2013 command line tools available
-call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" %platform%
+:: Download and compile Lua (or LuaJIT)
+if "%LUA%"=="luajit" (
+	set lj_dest_folder=c:\lj%LJ_SHORTV%
+	
+	if !LJ_SHORTV!==2.1 (
+		set lj_source_folder=c:\luajit-%LJ_VER%
+		git clone http://luajit.org/git/luajit-2.0.git !lj_source_folder!
+		c: && cd !lj_source_folder!\src
+		git checkout v2.1
+	) else (
+		%DOWNLOADER% http://luajit.org/download/LuaJIT-%LJ_VER%.zip
+		set lj_source_folder=!CD!\luajit-%LJ_VER%
+		"%SEVENZIP%" x LuaJIT-%LJ_VER%.zip
+		cd !lj_source_folder!\src
+	)
+	call msvcbuild.bat
 
-:: Download and compile Lua
-appveyor DownloadFile %LUAURL%/lua-%LUA_VER%.tar.gz
-7z x lua-%LUA_VER%.tar.gz
-7z x lua-%LUA_VER%.tar
-cd lua-%LUA_VER%
-appveyor DownloadFile https://github.com/Tieske/luawinmake/archive/master.zip
-7z x master.zip
-if not exist etc mkdir etc
-mv luawinmake-master\etc\winmake.bat %APPVEYOR_BUILD_FOLDER%\lua-%LUA_VER%\etc\winmake.bat
-call etc\winmake
-call etc\winmake install c:\lua%LUA_VER%
+	if not exist !lj_dest_folder! (
+		mkdir !lj_dest_folder!
+		mkdir !lj_dest_folder!\bin
+		mkdir !lj_dest_folder!\include
+		mkdir !lj_dest_folder!\lib
+	)
+
+	move !lj_source_folder!\src\luajit.exe !lj_dest_folder!\bin
+	move !lj_source_folder!\src\lua51.dll !lj_dest_folder!\bin
+	move !lj_source_folder!\src\lua51.lib !lj_dest_folder!\lib
+	for %%a in (lauxlib.h lua.h lua.hpp luaconf.h lualib.h) do ( copy "!lj_source_folder!\src\%%a" "!lj_dest_folder!\include" )
+
+	set LUA_DIR=!lj_dest_folder!
+) else (
+	%DOWNLOADER% %LUAURL%/lua-%LUA_VER%.tar.gz
+	"%SEVENZIP%" x lua-%LUA_VER%.tar.gz
+	"%SEVENZIP%" x lua-%LUA_VER%.tar
+	cd lua-%LUA_VER%
+	%DOWNLOADER% https://github.com/Tieske/luawinmake/archive/master.zip
+	"%SEVENZIP%" x master.zip
+	if not exist etc mkdir etc
+	mv luawinmake-master\etc\winmake.bat %APPVEYOR_BUILD_FOLDER%\lua-%LUA_VER%\etc\winmake.bat
+	call etc\winmake
+	call etc\winmake install c:\lua%LUA_VER%
+	set LUA_DIR=c:\lua%LUA_VER%
+)
 
 :: defines LUA_DIR so Cmake can find this Lua install
-set LUA_DIR=c:\lua%LUA_VER%
-set PATH=%PATH%;c:\lua%LUA_VER%\bin
-lua -v
+set PATH=!PATH!;!LUA_DIR!\bin
+call !LUA! -v
 
 :: Downloads and installs LuaRocks
 cd %APPVEYOR_BUILD_FOLDER%
-appveyor DownloadFile %LUAROCKS_URL%/luarocks-%LUAROCKS_VER%-win32.zip
-7z x luarocks-%LUAROCKS_VER%-win32.zip
+%DOWNLOADER% %LUAROCKS_URL%/luarocks-%LUAROCKS_VER%-win32.zip
+"%SEVENZIP%" x luarocks-%LUAROCKS_VER%-win32.zip
 cd luarocks-%LUAROCKS_VER%-win32
-call install.bat /LUA c:\lua%LUA_VER% /Q /LV %LUA_SHORTV%
+call install.bat /LUA %LUA_DIR% /Q /LV %LUA_SHORTV%
 set PATH=%PATH%;%ProgramFiles(x86)%\LuaRocks\%LUAROCKS_SHORTV%\;%ProgramFiles(x86)%\LuaRocks\systree\bin
 set LUA_PATH=%ProgramFiles(x86)%\LuaRocks\%LUAROCKS_SHORTV%\lua\?.lua;%ProgramFiles(x86)%\LuaRocks\%LUAROCKS_SHORTV%\lua\?\init.lua;%ProgramFiles(x86)%\LuaRocks\systree\share\lua\%LUA_SHORTV%\?.lua;%ProgramFiles(x86)%\LuaRocks\systree\share\lua\%LUA_SHORTV%\?\init.lua
 set LUA_CPATH=%ProgramFiles(x86)%\LuaRocks\systree\lib\lua\%LUA_SHORTV%\?.dll
 call luarocks --version
 
-cd %APPVEYOR_BUILD_FOLDER%
+endlocal & set LUA_DIR=%LUA_DIR% & set PATH=%PATH%
--- README.md
+++ README.md
@@ -1,13 +1,13 @@
-[![Build status](https://ci.appveyor.com/api/projects/status/subf1ydrutspjtk8?svg=true)](https://ci.appveyor.com/project/ignacio/lua-appveyor-example)
+[![Build status](https://ci.appveyor.com/api/projects/status/subf1ydrutspjtk8/branch/master?svg=true)](https://ci.appveyor.com/project/ignacio/lua-appveyor-example/branch/master)
 [![License](http://img.shields.io/badge/License-MIT-brightgreen.svg)](LICENSE)
 
 # lua-appveyor-example
 
 This sample repository is intended as a sample of how to integrate your Lua project with [AppVeyor](https://ci.appveyor.com/), providing you with continuous integration.
 
-This sample builds a matrix with Lua 5.1, 5.2 and 5.3. That means that each time you commit some changes, three jobs will be queued, so your code will be run using each of those Lua versions.
+This sample builds a matrix with Lua (versions 5.1, 5.2 and 5.3) and LuaJIT (2.0 and 2.1). That means that each time you commit some changes, five jobs will be queued, so your code will be run using each of those versions.
 So, each job will do the following:
-* Installs the appropiate Lua version (using [luawinmake](https://github.com/Tieske/luawinmake))
+* Installs the appropiate Lua (using [luawinmake](https://github.com/Tieske/luawinmake)) or LuaJIT version.
 * Installs [LuaRocks](https://rocks.moonscript.org/) (2.2.1)
 * Builds and installs a sample Lua module containing C and Lua parts
 * Runs tests using [Telescope](https://github.com/norman/telescope)
